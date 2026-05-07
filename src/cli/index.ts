d9dd2bcc763589227585c03b6484e3fcf5e7edc0
YelenaTor <Klaraenowak@gmail.com>
2026-05-07 14:21:48 +0200

    chore: Fixed GitHub Actions workflow, and bump to v0.2.1-testing.0

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,7 +6,7 @@ import { runInitForFramework } from "./init.js";
 import { parseShaderlabCli } from "./parse-args.js";
 import type { WriterReport } from "./writers/types.js";
 
-const VERSION = "0.2.0-testing.0";
+const VERSION = "0.2.1-testing.0";
 
 function printHelp(): void {
   // eslint-disable-next-line no-console
